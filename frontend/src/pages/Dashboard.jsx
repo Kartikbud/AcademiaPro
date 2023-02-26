5d5144af1bb771a6c778a99688a4d5fd5ff2b27f
rijwal <rijwalsangey@gmail.com>
2023-02-25 22:03:43 -0700

    hio

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,21 @@
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div class = "main">
+    <input type="checkbox" id="check">
+    <label for="check">
+    <i class="fas fa-bars" id="btn"></i>
+    <i class="fas fa-times" id="cancel"></i>
+</label>
+  <div class="sidebar">
+  <header>Menu</header>
+    <span><a href = "index.html" >Log Out</a></span>
+  </a>
+</div>
+</div>
+<div class = "images">
+    <img src = "images/logo1.png"  width = "550" height = "160" alt = "">
+</div>
+    
   )
 }
 
