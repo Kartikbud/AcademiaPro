105f149778351d59a6c40c68e6c0764420bb9f86
rijwal <rijwalsangey@gmail.com>
2023-02-26 01:31:12 -0700

    some string

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,24 @@
 function Dashboard() {
   return (
-  
-    <div class = "main">
-      <input type="checkbox" id="check"/>
-      <label for="check">
-        <i class="fas fa-bars" id="btn"></i>
-        <i class="fas fa-times" id="cancel"></i>
-      </label>
+    <>
+      <div class = "main">
+        <input type="checkbox" id="check"/>
+        <label for="check">
+          <i class="fas fa-bars" id="btn"></i>
+          <i class="fas fa-times" id="cancel"></i>
+        </label>
 
-      <div class="sidebar">
-        <header>Menu</header>
-        <span><a href = "index.html" >Log Out</a></span>
+        <div class="sidebar">
+          <header>Menu</header>
+          <span><a href = "index.html" >Log Out</a></span>
+        </div>
+
+        <div class = "images">
+          <img src = {require ("../images/logo1.png")} width = "210" height = "140" alt = ""/>
       </div>
 
-      <div class = "images">
-        <img src = {require ("../images/logo1.png")} width = "210" height = "140" alt = ""/>
-     </div>
-
-  </div>
+    </div>
+  </>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,34 @@
 function Login() {
   return (
- 
-    <div class = "homepage">
-        <div class = "box1">
-            <div class="login">
-                <form>
-                    <label>Returning User. Welcome Back</label>
-                    <input type="text" name="txt" placeholder="Name" required=""/>
-                    <input type="password" name="pswd" placeholder="Password" required=""/>
-                    <button><a href = "teachersignin.html" >Log In</a></button>
-                </form>
-                <label>New To Academia Pro. Create An Account For Free Today</label>
-                <button><a href = "signup.html" >Sign Up</a></button>
+    <>
+        <div class = "homepage">
+            <div class = "box1">
+                <div class="login">
+                    <form>
+                        <label>Returning User. Welcome Back</label>
+                        <input type="text" name="txt" placeholder="Name" required=""/>
+                        <input type="password" name="pswd" placeholder="Password" required=""/>
+                        <button><a href = "teachersignin.html" >Log In</a></button>
+                    </form>
+                    <label>New To Academia Pro. Create An Account For Free Today</label>
+                    <button><a href = "signup.html" >Sign Up</a></button>
+                </div>
             </div>
+
+            <div class = "text">
+                
+                <h1> Engage.</h1>
+                <h1> Encourage. </h1>
+                <h1> Succeed.</h1>
+            </div>
+
+            <div class = "images1">
+                <img src = {require ("../images/logo1.png")} width = "400" height = "266" alt = ""/>
+            </div>
+
         </div>
-        <div class = "images1">
-            <img src = "logo5.png"  width = "500" height = "180" alt = "wadwad"/>
-            <h1>Our Mission: Sum Messagewadawdwadwaawd</h1>
-        </div>
-    </div>
+
+    </>
   )
 }
 
