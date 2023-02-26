4c08ab0278ba6fc941100cf03e3b3b14a07e2b8b
Kartik <kpbudihal@gmail.com>
2023-02-25 22:06:21 -0700

    lkj

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,11 @@
 function Dashboard() {
   return (
     <div class = "main">
-    <input type="checkbox" id="check">
-    <label for="check">
-    <i class="fas fa-bars" id="btn"></i>
-    <i class="fas fa-times" id="cancel"></i>
-</label>
+      <input type="checkbox" id="check"/>
+      <label for="check">
+        <i class="fas fa-bars" id="btn"></i>
+        <i class="fas fa-times" id="cancel"></i>
+      </label>
   <div class="sidebar">
   <header>Menu</header>
     <span><a href = "index.html" >Log Out</a></span>
@@ -13,7 +13,7 @@ function Dashboard() {
 </div>
 </div>
 <div class = "images">
-    <img src = "images/logo1.png"  width = "550" height = "160" alt = "">
+  <img src = "images/logo1.png"  width = "550" height = "160" alt = ""/>
 </div>
     
   )
